a7b0058a8ea9e26bc6d49fcca5fcbeb60ef2e344
Riko Tertsch <riko.tertsch@gmail.com>
2022-09-12 21:49:19 +0200

    Question 3

--- question1.py
+++ question1.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pandas
 
-print("Question 1: " + np.__version__)
+print("Question 1:", np.__version__)
 
-pandas.read_csv("data.csv")
+data = pandas.read_csv("data.csv")
 
+print("Question 2:", len(data))
+
+value_counts = data['Make'].value_counts()
+top3 = value_counts.head(3)
+names = top3.keys().tolist()
+
+print("Question 3:", ", ".join(names))
